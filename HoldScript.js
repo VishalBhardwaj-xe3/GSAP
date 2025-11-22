0cea0ab239152164c3b87f9555c7b0460d741e89
Vishal Bhardwaj <vishalbhardwaj0571@gmail.com>
2025-11-22 13:44:17 +0530

    String animation like guitar

--- HoldScript.js
+++ HoldScript.js
@@ -13,3 +13,28 @@ gsap.to("#box2 h1", {
 });
 
 
+// String animation like guitar
+var initialPath = `M 10 100 Q 250 100 490 100`;
+
+var finalPath = `M 10 100 Q 250 100 490 100`;
+
+var string = document.querySelector("#string");
+
+string.addEventListener("mousemove", function (dets) {
+  path = `M 10 100 Q ${dets.x} ${dets.y} 490 100`;
+
+  gsap.to("svg path", {
+    attr: { d: path },
+    duration: 0.2,
+    ease: "power3.out",
+  });
+});
+
+string.addEventListener("mouseleave", function () {
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.5,
+    ease: "elastic.out(1,0.8)",
+  });
+});
+
